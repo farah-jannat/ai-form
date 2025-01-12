0c5dc093ad0ef8e0e27ae0e27f56d5e17725e44c
“farah-jannat” <farahjannatmithila@gmail.com>
2025-01-12 22:50:21 +0600

    Add daisyui buttons

--- app/page.js
+++ app/page.js
@@ -6,5 +6,12 @@ export default function Home() {
     <div>
       hello world
       <Button>subscribe</Button>
+      <button className="btn">Button</button>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-secondary">Secondary</button>
+      <button className="btn btn-accent">Accent</button>
+      <button className="btn btn-ghost">Ghost</button>
+      <button className="btn btn-link">Link</button>
     </div>);
 }

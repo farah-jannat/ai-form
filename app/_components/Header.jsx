24b2a8b7f6eac1eef83f8a574bd82f95d6974095
“farah-jannat” <farahjannatmithila@gmail.com>
2025-01-13 11:47:10 +0600

    feat : add authentication

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,13 +1,29 @@
+'use client'
 import { Button } from '@/components/ui/button'
+import { SignInButton, UserButton, useUser } from '@clerk/nextjs'
 import Image from 'next/image'
 import React from 'react'
+import Link from 'next/link'
 
 function Header() {
+
+    const { user, isSignedIn } = useUser();
     return (
         <div className='p-5 border-b shadow-sm'>
             <div className='flex justify-between'>
                 <Image src={'/logo.svg'} width={180} height={50} alt='logo' />
-                <Button>Get Started</Button>
+                {isSignedIn ?
+
+                    <div className='flex items-center gap-5'>
+                        <Link href={'/dashboard'}>
+                            <Button variant="outline">Dashboard</Button>
+                        </Link>
+                        <UserButton />
+                    </div> :
+                    <SignInButton>
+                        <Button>Get started</Button>
+                    </SignInButton>
+                }
             </div>
         </div>
     )

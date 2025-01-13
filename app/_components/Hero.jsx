3e7e2edb613143271de5b098f31c0ba1ec733bb5
“farah-jannat” <farahjannatmithila@gmail.com>
2025-01-13 09:56:06 +0600

    fix: fix contens

--- app/_components/Hero.jsx
+++ app/_components/Hero.jsx
@@ -6,25 +6,25 @@ function Hero() {
             <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
                 <div className="mx-auto max-w-xl text-center">
                     <h1 className="text-3xl font-extrabold sm:text-5xl">
-                        Understand User Flow.
-                        <strong className="font-extrabold text-red-700 sm:block"> Increase Conversion. </strong>
+                        Create Your Form
+                        <strong className="font-extrabold text-primary sm:block"> In Seconds Not Hours </strong>
                     </h1>
 
-                    <p className="mt-4 sm:text-xl/relaxed">
+                    <p className="mt-4 sm:text-xl/relaxed text-gray-500">
                         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt illo tenetur fuga ducimus
                         numquam ea!
                     </p>
 
                     <div className="mt-8 flex flex-wrap justify-center gap-4">
                         <a
-                            className="block w-full rounded bg-red-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-red-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
+                            className="block w-full rounded bg-primary px-12 py-3 text-sm font-medium text-white shadow hover:bg-purple-600 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
                             href="#"
                         >
-                            Get Started
+                            + Create AI Form
                         </a>
 
                         <a
-                            className="block w-full rounded px-12 py-3 text-sm font-medium text-red-600 shadow hover:text-red-700 focus:outline-none focus:ring active:text-red-500 sm:w-auto"
+                            className="block w-full rounded px-12 py-3 text-sm font-medium text-primary shadow hover:text-purple-600 focus:outline-none focus:ring active:text-red-500 sm:w-auto"
                             href="#"
                         >
                             Learn More

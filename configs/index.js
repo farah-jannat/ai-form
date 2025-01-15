553df232bb221fefe603d2b90267f989a970bd9b
“farah-jannat” <farahjannatmithila@gmail.com>
2025-01-15 16:13:05 +0600

    fix : export db

--- configs/index.js
+++ configs/index.js
@@ -3,4 +3,4 @@ import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from './schema';
 
 const sql = neon(process.env.NEXT_PUBLIC_DATABASE_URL_CONFIG);
-const db = drizzle({ client: sql }, { schema });
+export const db = drizzle({ client: sql }, { schema });

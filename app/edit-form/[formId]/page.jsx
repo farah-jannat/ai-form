1a7ea248618cbe282308a2601e0d3c4095fbf7d9
“farah-jannat” <farahjannatmithila@gmail.com>
2025-01-15 16:32:17 +0600

    Add getFromdata

--- app/edit-form/[formId]/page.jsx
+++ app/edit-form/[formId]/page.jsx
@@ -1,6 +1,21 @@
+'use client'
+import { db } from '@/configs'
+import { jsonForms } from '@/configs/schema'
+import { useUser } from '@clerk/nextjs'
+import { eq } from 'drizzle-orm'
 import React from 'react'
 
-function EditForm() {
+function EditForm({ params }) {
+
+    const { user } = useUser()
+
+    const GetFormData = async () => {
+        const result = await db.select().from(jsonForms).where(and(eq(jsonForms.id, params?.formId), eq(jsonForms.createdBy, user?.primaryEmailAddress?.emailAddress)))
+    }
+
+
+
+
     return (
         <div>EditForm</div>
     )

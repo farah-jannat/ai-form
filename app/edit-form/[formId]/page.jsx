2ca0e99e9507290254c6a31384df7a01d5380897
“farah-jannat” <farahjannatmithila@gmail.com>
2025-01-17 09:28:51 +0600

    Update edit-form

--- app/edit-form/[formId]/page.jsx
+++ app/edit-form/[formId]/page.jsx
@@ -1,23 +1,50 @@
 'use client'
 import { db } from '@/configs'
-import { jsonForms } from '@/configs/schema'
+import { JsonForms } from '@/configs/schema'
 import { useUser } from '@clerk/nextjs'
-import { eq } from 'drizzle-orm'
-import React from 'react'
+import { and, eq } from 'drizzle-orm'
+import React, { useEffect, useState } from 'react'
+import { useParams } from "next/navigation";
 
-function EditForm({ params }) {
+function EditForm() {
 
     const { user } = useUser()
+    const params = useParams();
+    const [jsonForm, setJsonForm] = useState([])
+
+
+
+
+    useEffect(() => {
+        user && GetFormData();
+    }, [user])
 
     const GetFormData = async () => {
-        const result = await db.select().from(jsonForms).where(and(eq(jsonForms.id, params?.formId), eq(jsonForms.createdBy, user?.primaryEmailAddress?.emailAddress)))
+        const result = await db.select().from(JsonForms)
+            .where(and(eq(JsonForms.id, params?.formId),
+                eq(JsonForms.createdBy, user?.primaryEmailAddress?.emailAddress)));
+
+        // console.log('jsonform ', result[0].jsonform)
+        const cleanedResponse = result[0].jsonform.replace("```json", "").replace("```", "");
+        // console.log('cleanedresponse', cleanedResponse)
+        setJsonForm(JSON.parse(cleanedResponse))
+        // console.log('parse json', JSON.parse(cleanedResponse))
+
+        // console.log('usestate jsonform', jsonForm)
+
+
     }
 
 
-
-
     return (
-        <div>EditForm</div>
+        <div className='p-10 bg-slate-600'>
+            <div className='grid grid-cols-1 bg-red-700 md:grid-cols-3'>
+                <div>
+                    Controller
+                </div>
+                <div className='md:col-end-2 bg-blue-500'>Form</div>
+            </div>
+        </div>
     )
 }
 

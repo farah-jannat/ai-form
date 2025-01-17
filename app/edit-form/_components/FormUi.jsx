e1ad0f806e3c05e0fcac46ed1865f2282c415474
“farah-jannat” <farahjannatmithila@gmail.com>
2025-01-17 20:51:46 +0600

    feat: add checkbox , radiogroup

--- app/edit-form/_components/FormUi.jsx
+++ app/edit-form/_components/FormUi.jsx
@@ -1,6 +1,8 @@
 import { Input } from '@/components/ui/input'
 import React from 'react'
 
+import { Checkbox } from "@/components/ui/checkbox"
+
 import { Label } from "@/components/ui/label"
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
 
@@ -15,7 +17,7 @@ import {
 
 function FormUi({ jsonForm }) {
     return (
-        <div className='border p-5 md:w-[600px]'>
+        <div className='border p-5 md:w-[600px] rounded-lg'>
             <h2 className='font-bold text-center text-2xl'>{jsonForm.formTitle}</h2>
             <h2 className='text-sm text-gray-400 text-center'>{jsonForm.formHeading}</h2>
 
@@ -37,30 +39,50 @@ function FormUi({ jsonForm }) {
                                 </SelectContent>
                             </Select>
                         </div>
-                        : field.fieldType == 'radio' ?
-                            <div>
-
-                                <RadioGroup defaultValue="option-one">
-                                    {field.options.map((item, index) => (
-                                        <div className="flex items-center space-x-2">
-                                            <RadioGroupItem value="option-one" id="option-one" />
-                                            <Label htmlFor="option-one">Option One</Label>
-                                        </div>
-                                    ))}
-
-
-                                </RadioGroup>
-
-                            </div>
-
-
-                            : < div className='my-3'>
+                        : field.fieldType == 'checkbox' ?
+                            <div className='my-3'>
                                 <label className='text-xs text-gray-500'>{field.label}</label>
-                                <Input type={field?.type}
-                                    placeholder={field?.placeholder}
-                                // name={field?.fieldName}
-                                />
+                                {field?.options ? field?.options?.map((item, index) => (
+                                    <div className='flex gap-2'>
+                                        <Checkbox />
+                                        <h2>{item.label}</h2>
+
+                                    </div>
+
+                                ))
+                                    :
+
+                                    <div className='flex  items-center gap-2'>
+                                        <Checkbox />
+                                        <h2>{field.label}</h2>
+                                    </div>
+                                }
                             </div>
+                            : field.fieldType == "radiogroup" ?
+                                <div>
+                                    <label className='text-xs text-gray-500'>{field.label}</label>
+
+                                    <RadioGroup>
+                                        {field?.options?.map((item, index) => (
+                                            <div key={index} className="flex items-center space-x-2">
+                                                <RadioGroupItem value={item} id={item} />
+                                                <Label htmlFor={item}>{item}</Label>
+                                            </div>
+                                        ))}
+
+
+                                    </RadioGroup>
+
+                                </div>
+
+
+                                : < div className='my-3'>
+                                    <label className='text-xs text-gray-500'>{field.label}</label>
+                                    <Input type={field?.type}
+                                        placeholder={field?.placeholder}
+                                    // name={field?.fieldName}
+                                    />
+                                </div>
                     }
                 </div>
 

1247ed6c8e004091c218b1e622d3862463a9b3ec
“farah-jannat” <farahjannatmithila@gmail.com>
2025-01-19 13:51:54 +0600

    Update schema

--- configs/schema.js
+++ configs/schema.js
@@ -4,6 +4,9 @@ const { serial, varchar, pgTable, text } = require("drizzle-orm/pg-core");
 export const JsonForms = pgTable('JsonForms', {
     id: serial('id').primaryKey(),
     jsonform: text('jsonform').notNull(),
+    theme: varchar('theme'),
+    background: varchar('background'),
+    style: varchar('style'),
     createdBy: varchar('createdBy').notNull(),
     createdAt: varchar('createdAt').notNull()
 

ee11263974ece17d457ad528b6c360f8cbb0bb87
“farah-jannat” <farahjannatmithila@gmail.com>
2025-01-21 21:57:33 +0600

    Add form-data in userrespnses db

--- app/edit-form/_components/FormUi.jsx
+++ app/edit-form/_components/FormUi.jsx
@@ -1,11 +1,12 @@
 'use client'
 import { Input } from '@/components/ui/input'
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 
 import { Checkbox } from "@/components/ui/checkbox"
 
 import { Label } from "@/components/ui/label"
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
+import { db } from '@/configs'
 
 
 import {
@@ -16,10 +17,16 @@ import {
     SelectValue,
 } from "@/components/ui/select"
 import FieldEdit from './FieldEdit'
+import { userResponses } from '@/configs/schema'
+import moment from 'moment'
+import { toast } from 'sonner'
 
-function FormUi({ jsonForm, onFieldUpdate, deleteField, selectedTheme, editable = true, selectedStyle }) {
+function FormUi({ jsonForm, onFieldUpdate, deleteField, selectedTheme, editable = true, selectedStyle, formId }) {
+
 
     const [formData, setFormData] = useState();
+    let formRef = useRef();
+
     const handleInputChange = (event) => {
         const { name, value } = event.target
         // console.log('name of and value', name, value)
@@ -29,16 +36,60 @@ function FormUi({ jsonForm, onFieldUpdate, deleteField, selectedTheme, editable 
             [name]: value
         })
     }
+    const handleSelectChange = (name, value) => {
+        setFormData({
+            ...formData,
+            [name]: value
+        })
+    }
 
-    const onFormSubmit = (event) => {
+    const handleCheckboxChange = (fieldName, itemName, value) => {
+        // console.log("form handleCheckChange", fieldName, itemName, value)
+        const list = formData?.[fieldName] ? formData?.[fieldName] : []
+        // console.log('list', list)
+        if (value) {
+            list.push({
+                label: itemName,
+                value: value
+            })
+            setFormData({
+                ...formData,
+                [fieldName]: list
+            })
+        } else {
+            const result = list.filter((item) => item.label == itemName)
+            setFormData({
+                ...formData,
+                [fieldName]: list
+            })
+        }
+        // console.log('formdata', formData)
+
+    }
+
+    const onFormSubmit = async (event) => {
         console.log('fomdata', formData)
         event.preventDefault();
+        const result = await db.insert(userResponses)
+            .values({
+                jsonResponses: formData,
+                createdAt: moment().format('DD/MM/YYYY'),
+                formRef: formId
+            })
+        if (result) {
+            formRef.reset();
+            toast('Responses submitted successfully !')
+
+        } else {
+            toast('Internal error while saving your form')
+        }
     }
 
 
 
     return (
         <form
+            ref={(e) => formRef = e}
             onSubmit={onFormSubmit}
             className='border p-5 md:w-[600px] rounded-lg' data-theme={selectedTheme} style={
 
@@ -55,7 +106,7 @@ function FormUi({ jsonForm, onFieldUpdate, deleteField, selectedTheme, editable 
 
                     {field.fieldType == 'select' ?
                         <div className='my-3 w-full'>
-                            <Select>
+                            <Select required={field?.required} onValueChange={(value) => handleSelectChange(field.fieldName, value)}>
                                 <SelectTrigger className="w-full">
                                     <SelectValue placeholder={field.label} />
                                 </SelectTrigger>
@@ -73,7 +124,7 @@ function FormUi({ jsonForm, onFieldUpdate, deleteField, selectedTheme, editable 
                                 <label className='text-xs text-gray-500'>{field.label}</label>
                                 {field?.options ? field?.options?.map((item, index) => (
                                     <div key={index} className='flex gap-2'>
-                                        <Checkbox required={field?.required} />
+                                        <Checkbox required={field?.required} onCheckedChange={(v) => handleCheckboxChange(field.label, item, v)} />
                                         <h2>{item}</h2>
 
                                     </div>
@@ -82,7 +133,7 @@ function FormUi({ jsonForm, onFieldUpdate, deleteField, selectedTheme, editable 
                                     :
 
                                     <div className='flex  items-center gap-2'>
-                                        <Checkbox />
+                                        <Checkbox required={field?.required} />
                                         <h2>{field.label}</h2>
                                     </div>
                                 }
@@ -91,10 +142,12 @@ function FormUi({ jsonForm, onFieldUpdate, deleteField, selectedTheme, editable 
                                 <div className='w-full'>
                                     <label className='text-xs text-gray-500'>{field.label}</label>
 
-                                    <RadioGroup required={field?.required}>
+                                    <RadioGroup required={field?.required} >
                                         {field?.options?.map((item, index) => (
                                             <div key={index} className="flex items-center space-x-2">
-                                                <RadioGroupItem value={item} id={item} />
+                                                <RadioGroupItem value={item} id={item}
+                                                    onClick={() => handleSelectChange(field.label, item)}
+                                                />
                                                 <Label htmlFor={item}>{item}</Label>
                                             </div>
                                         ))}

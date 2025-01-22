3a515dc26fc7d5d162be353634699696baf41514
“farah-jannat” <farahjannatmithila@gmail.com>
2025-01-22 19:10:09 +0600

    fix: link in sidenav component

--- app/dashboard/_components/SideNav.jsx
+++ app/dashboard/_components/SideNav.jsx
@@ -5,6 +5,7 @@ import { usePathname } from 'next/navigation'
 import React, { useEffect } from 'react'
 import { Button } from '@/components/ui/button'
 import { Progress } from "@/components/ui/progress"
+import Link from 'next/link'
 
 
 function SideNav() {
@@ -46,10 +47,10 @@ function SideNav() {
         <div className='h-screen shadow-md border'>
             <div className='p-5'>
                 {menuList.map((menu, index) => (
-                    <h2 key={index} className={`flex items-center gap-3 p-4 hover:bg-primary hover:text-white rounded-lg mb-3 cursor-pointer text-gray-500 ${path == menu.path && 'bg-primary text-white'}`}>
+                    <Link href={menu.path} key={index} className={`flex items-center gap-3 p-4 hover:bg-primary hover:text-white rounded-lg mb-3 cursor-pointer text-gray-500 ${path == menu.path && 'bg-primary text-white'}`}>
                         <menu.icon />
                         {menu.name}
-                    </h2>
+                    </Link>
                 ))}
             </div>
             <div className='fixed bottom-20 p-6 w-64'>
